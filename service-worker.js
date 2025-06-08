421ebb274332e7d480c44d5ed93bfe373d4577ca
Xi Lu <448153639@qq.com>
2025-06-07 21:50:44 -0400

    Init

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './favicon.svg',
+    './min2phase.js'
+];
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (!cacheWhitelist.includes(cache)) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+self.addEventListener('message', event => {
+    if (event.data === 'ud') {
+      caches.open(CACHE_NAME).then(cache => {
+        urlsToCache.forEach(url => {
+          fetch(url).then(response => cache.put(url, response));
+        });
+      });
+    }
+  });
+  
